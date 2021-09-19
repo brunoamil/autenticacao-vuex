ce414a146794ca981a92b91d8416fe282091c122
Bruno <bruno-amil@hotmail.com>
2021-09-19 11:47:18 -0300

    refatorando a rota

--- src/views/Gerentes.vue
+++ src/views/Gerentes.vue
@@ -23,6 +23,12 @@ export default {
     this.$http.get('gerentes')
     .then(response => this.gerentes = response)
     .catch(erro => console.log(erro));
+  },
+  beforeRouteEnter(to, from, next){
+    if(!this.$store.state.token){
+      next({ name: 'login'});
+    }
+    next();
   }
 };
 </script>
