b22591f72df4348ee2a4cb2cfd384d25fc2c48a2
Bruno <bruno-amil@hotmail.com>
2021-09-19 10:55:36 -0300

    alteracoes com vuex

---
+++ src/components/BarraNavegacaoLogado.vue
@@ -0,0 +1,25 @@
+<template>
+   <ul class="navbar-nav mr-auto" v-if="usuarioEstaLogado">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/gerentes" class="nav-link">
+              Gerentes
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link" @click.prevent="efeatuarLogout">Logout</a>
+          </li>
+        </ul>
+</template>
+<script>
+export default {
+  methods: {
+    efetuarLogout () {
+      localStorage.removeItem('token');
+      this.$router.push({ name: 'login'});
+    }
+  }
+};
+</script>
